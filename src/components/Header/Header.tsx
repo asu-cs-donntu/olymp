6254ec652057dbbdf4e3b6d996a0b0ff0bf40f90
i888844 <i888844@yandex.ru>
2025-03-07 13:59:35 +0300

    alerts & textcards modifications

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,35 +7,28 @@ const Header: React.FC = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [alertText, setAlertText] = useState("");
 
-  const handleContactsClick = () => {
-    setAlertText("Для связи с организаторами используйте e-mail cafedraasu@yandex.ru\n\nСсылка на тесты будет выслана по почте на зарегистрированные адреса 05.04.2025 с 9:00 до 9:15. В случае не получения ссылки пишите организаторам по адресу cafedraasu@yandex.ru");
+  const handleContactsClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setAlertText("Для связи с организаторами используйте e-mail cafedraasu@yandex.ru");
     setShowAlert(true);
   };
 
-  const handleResultsClick = () => {
-    setAlertText("В данный момент просмотр результатов недоступен.\n\nСсылка на тесты будет выслана по почте на зарегистрированные адреса 05.04.2025 с 9:00 до 9:15. В случае не получения ссылки пишите организаторам по адресу cafedraasu@yandex.ru");
+  const handleResultsClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setAlertText("В данный момент просмотр результатов недоступен.");
     setShowAlert(true);
   };
 
-  const handleRegistrationClick = () => {
-    const now = new Date().getTime();
-    const REG_END = new Date("2025-04-03T23:59:59+03:00").getTime();
-
-    let message = "В данный момент регистрация недоступна";
-
-    if (now <= REG_END) {
-      message = "Для регистрации перейдите по следующему адресу: https://forms.yandex.ru/u/67c551ac068ff08c98a73abf/";
-    }
-
-    setAlertText(`${message}\n\nСсылка на тесты будет выслана по почте на зарегистрированные адреса 05.04.2025 с 9:00 до 9:15. В случае не получения ссылки пишите организаторам по адресу cafedraasu@yandex.ru`);
-    setShowAlert(true);
+  const handleRegistrationClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    window.open('https://forms.yandex.ru/u/67c551ac068ff08c98a73abf/', '_blank');
   };
 
   return (
       <div className="main-container">
         <nav className="navbar navbar-expand-lg">
           <div className="container">
-            <a href="https://donntu.ru/" target="_blank">
+            <a href="https://donntu.ru/" target="_blank" rel="noreferrer">
               <div className="logo">
                 <img src={LogoDonNTU} alt="Логотип ДонНТУ"/>
                 <p>
@@ -44,8 +37,8 @@ const Header: React.FC = () => {
                 </p>
               </div>
             </a>
-          <button
-              className="navbar-toggler"
+            <button
+                className="navbar-toggler"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#navbarTogglerDemo02"
